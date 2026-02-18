5eb5ac1798d7bba0270f1fe9cc6819faeeb786f1
Sujal Chouhan <sujalchouhan@jklu.edu.in>
2026-02-18 20:07:52 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,26 @@
+// Code for a Pallindrome number.
+
+#include<stdio.h>
+
+int main(){
+    int a,o,r=0,rem;
+    printf("Enter any number: ");
+    scanf("%d",&a);
+
+    o = a;
+
+    while(a!=0){
+        rem = a%10;
+        r = r*10 + rem;
+        a = a/10;
+    }
+
+    if(o==r){
+        printf("%d is a Pallindrome number.",o);
+    }
+    else{
+        printf("%d is not a Pallindrome number.",o);
+    }
+
+    return 0;
+}
