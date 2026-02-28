348cb24f82386e91bdd359d6f87cd967bdc2cc3c
Sujal Chouhan <sujalchouhan@jklu.edu.in>
2026-02-28 21:30:38 +0530

    Add files via upload

---
+++ q11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    int a;
+    printf("Enter any Number: ");
+    scanf("%d",&a);
+
+    if(a>0 &&(a&(a-1)) == 0){
+        printf("%d is a Power of Two.\n",a);
+    } else{
+        printf("%d is NOT a Power of Two.\n", a);
+    }
+
+    return 0;
+}
